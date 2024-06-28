d4236b80707c1222bbfdced922f8d2aa8bc0bcc5
Christian Salon <christian.salon@whitecloak.com>
2024-06-28 21:54:02 +0800

    Resolve issue when trying to render the ball when it was already removed

--- lib/ball_game/data/model/ball.dart
+++ lib/ball_game/data/model/ball.dart
@@ -8,6 +8,7 @@ class Ball extends CircleComponent
   static const double speed = 100.0;
 
   Vector2 direction;
+  bool markedForRemoval = false;
 
   Ball({required Vector2 position})
       : direction = (Vector2.random() - Vector2(0.5, 0.5)).normalized(),
@@ -28,16 +29,23 @@ class Ball extends CircleComponent
   void update(double dt) {
     super.update(dt);
 
+    // Remove the ball if it's marked for removal
+    if (markedForRemoval) {
+      removeFromParent();
+      return;
+    }
+
     position += direction * speed * dt;
 
     if (position.x < 0 || position.x > gameRef.size.x) direction.x *= -1;
     if (position.y < 0 || position.y > gameRef.size.y) direction.y *= -1;
 
+    // Mark the ball for removal if it's outside the screen
     if (position.x < 0 ||
         position.x > gameRef.size.x ||
         position.y < 0 ||
         position.y > gameRef.size.y) {
-      removeFromParent();
+      markedForRemoval = true;
     }
   }
 
